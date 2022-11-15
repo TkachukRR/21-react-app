7e93bdc2bbd34a9afb308cb88b9628d7ed1783bb
TkachukRR <tkachukrr@gmail.com>
2022-11-15 12:40:31 +0100

    changed project list

--- src/modules/Projects/Projects.js
+++ src/modules/Projects/Projects.js
@@ -1,22 +1,34 @@
 import "./Projects.css";
 
 const Projects = ({ projects }) => (
-  <ol>
+  <ol className="project__list">
     {projects.map(({ title, url, urlGit, stack }) => (
-      <li key={url}>
-        <h2>{title}</h2>
-        <a href={url}>{url}</a>
-        <a href={urlGit}>{urlGit}</a>
-        <p>
-          [
-          <ul>
-            {stack.map((technology) => (
-              <li>{technology}</li>
-            ))}
-          </ul>
-          ]
-        </p>
-      </li>
+        <li key={url} className="project__item">
+          <h2 className="project__title">{title}</h2>
+
+          <div className="project__info">
+            <div className="project__links">
+              <a href={url} className="project__url">
+                {url}
+              </a>
+              <a href={urlGit} className="project__url-git">
+                {urlGit}
+              </a>
+            </div>
+
+            <div className="project__stack">
+              [
+              <ul className="stack__list">
+                {stack.map((technology) => (
+                  <li key={technology} className="stack__item">
+                    {technology}
+                  </li>
+                ))}
+              </ul>
+              ]
+            </div>
+          </div>
+        </li>
     ))}
   </ol>
 );
